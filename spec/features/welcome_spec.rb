011028950b5e4b5b6d85eeaa3c62133e5f533fad
Bruno Faboci <brunofaboci@gmail.com>
2021-06-09 15:53:48 -0300

    create test for welcome#index (link to _cadastro de clientes_)

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -2,7 +2,12 @@ require 'rails_helper'
 
 RSpec.feature "Welcomes", type: :feature do
   scenario 'Mostrar Boas Vindas' do
-    visit('/')
+    visit(root_path)
     expect(page).to have_content('Welcome')
+  end
+
+  scenario 'Virificar link p/ cadastro clientes' do
+    visit(root_path)
+    expect(find('ul li')).to have_link('Cadastro de Clientes')  
   end
 end
