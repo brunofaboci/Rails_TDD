6e642e8aa7c4b6b3488aea03eee73652e6aebd52
Bruno Faboci <brunofaboci@gmail.com>
2021-06-10 11:55:39 -0300

    create test for customers(index/new)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+
+  resources :customers
+
   root to: 'welcome#index'
 end
--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -6,7 +6,7 @@ RSpec.feature "Welcomes", type: :feature do
     expect(page).to have_content('Welcome')
   end
 
-  scenario 'Virificar link p/ cadastro clientes' do
+  scenario 'Verificar link p/ cadastro clientes' do
     visit(root_path)
     expect(find('ul li')).to have_link('Cadastro de Clientes')  
   end
